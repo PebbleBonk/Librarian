d064339f15a5908a9ecaf52a6a5af375ced954cb
Olli Riikonen <olli.m.riikonen@aalto.fi>
2020-03-31 09:38:42 +0300

    Add InitialisationException

--- librarian/exceptions.py
+++ librarian/exceptions.py
@@ -1,3 +1,8 @@
 class EnvironmentVariableLoadException(Exception):
     def __init__(self,*args,**kwargs):
         super().__init__(*args, **kwargs)
+
+
+class InitialisationError(Exception):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args, **kwargs)
